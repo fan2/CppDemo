19f74b247b052aaa63493f0d2d1bafea7625ffff
fan2 <challenge4fun@qq.com>
2017-08-18 08:22:52 +0800

    rename is_derived_from.hpp to is_kind_of.hpp and typeid(RTTI) test.

--- Utilities/Utilities/main.cpp
+++ Utilities/Utilities/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+#include <typeinfo>
 
 #include <type_traits>
-#include "is_derived_from.hpp"
+#include "is_kind_of.hpp"
 
 class A {};
 class B : A {};
@@ -19,7 +20,22 @@ class D : public B {};
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    // std::is_base_of
+    // typeid(RTTI)：获取类名
+    const std::type_info& tiA = typeid(A);
+    const std::type_info& tiB = typeid(B);
+    const std::type_info& tiC = typeid(C);
+    const std::type_info& tiD = typeid(D);
+    
+    std::cout << "tiA.name: " << tiA.name() << std::endl;
+    std::cout << "tiB.name: " << tiB.name() << std::endl;
+    std::cout << "tiC.name: " << tiC.name() << std::endl;
+    std::cout << "tiD.name: " << tiD.name() << std::endl;
+    D d;
+    std::cout << "typeid(d).name: " << typeid(d).name() << std::endl;
+    // std::type_info::hash_code since C++11
+    // std::type_index since C++11
+    
+    // std::is_base_of：判断是否是基类
     //  template< class Base, class Derived >
     std::cout << std::boolalpha;
     std::cout << "a2b: " << std::is_base_of<A, B>::value << '\n';
@@ -27,11 +43,11 @@ int main(int argc, const char * argv[]) {
     std::cout << "c2b: " << std::is_base_of<C, B>::value << '\n';
     std::cout << "same type: " << std::is_base_of<C, C>::value << '\n';
     
-    // Utilities::is_derived_from
+    // Utilities::is_kind_of：判断是否是继承
     //  template <typename Derived, typename Base>
     //  Semantic Issue: Cannot cast 'B' to its private base class 'A'
-    // std::cout << "b2a: " << Utilities::is_base_of<B, A>::value << '\n';
-    std::cout << "D:B = " << Utilities::is_derived_from<D, B>::value << '\n';
+    // std::cout << "b2a: " << Utilities::is_kind_of<B, A>::value << '\n';
+    std::cout << "D:B = " << Utilities::is_kind_of<D, B>::value << '\n';
     
     return 0;
 }
