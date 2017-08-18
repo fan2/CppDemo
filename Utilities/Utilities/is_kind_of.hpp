a5199633afc3befc9aa1828bf7f5a9b04c4388d7
fan2 <challenge4fun@qq.com>
2017-08-18 08:22:22 +0800

    rename is_derived_from.hpp to is_kind_of.hpp and typeid(RTTI) test.

--- Utilities/Utilities/is_kind_of.hpp
+++ Utilities/Utilities/is_kind_of.hpp
@@ -1,5 +1,5 @@
 //
-//  is_derived_from.hpp
+//  is_kind_of.hpp
 //  CppLib
 //
 //  Created by faner on 2017/8/17.
@@ -12,21 +12,32 @@
 // [How does `is_base_of` work?](https://stackoverflow.com/questions/2910979/how-does-is-base-of-work)
 // [由is_base_of看C++中的SFINAE](http://blog.csdn.net/zhangjun03402/article/details/48626957)
 
-#ifndef is_derived_from_hpp
-#define is_derived_from_hpp
+#ifndef is_kind_of_hpp
+#define is_kind_of_hpp
+
+#ifdef __OBJC__
+
+- (BOOL)isKindOfClass:(Class)aClass
+{
+    return [self class]==aClass;
+}
+
+#endif
+
+// boost/type_traits/is_base_of.hpp
 
 // test if Derived is derived from Base or if both are the same non-union class
 // if class Derived : class Base, then D is-a B
 namespace Utilities {
     // template< class Base, class Derived >
     template <typename D, typename B>
-    struct is_derived_from {
+    struct is_kind_of {
         struct yes_type {enum {value = true}; char padding[1];};
         struct no_type {enum {value = false}; int padding[2];};
-        static yes_type is_derived_from_test(B*);    // empty function body
-        static no_type is_derived_from_test(...);    // empty function body
-        enum {value = (sizeof(is_derived_from_test(static_cast<D*>(0))) == sizeof(yes_type))};
+        static yes_type is_kind_of_test(B*);    // empty function body
+        static no_type is_kind_of_test(...);    // empty function body
+        enum {value = (sizeof(is_kind_of_test(static_cast<D*>(0))) == sizeof(yes_type))};
     };
 };
 
-#endif /* is_derived_from_hpp */
+#endif /* is_kind_of_hpp */
